4667c7810116eb081fa75a0ba57723e4b733006e
Keichi Takahashi <keichi.t@me.com>
2012-03-15 00:48:51 +0900

    Refactored some code.

--- mandelbrot/main.cpp
+++ mandelbrot/main.cpp
@@ -8,33 +8,47 @@
 
 #include <iostream>
 #include <fstream>
+#include <cmath>
 #include <GLUT/glut.h>
+
+#define MAX_SHADER_LOG_SIZE		(1024)
 
 static GLuint program;
 static GLuint buffer;
 static int levelValue = 80;
 
-float vertex[] =
-{
+float vertex[] = {
     -1.0, 1.0, 0.0,
     1.0, 1.0, 0.0,
     -1.0, -1.0, 0.0,
     1.0, -1.0, 0.0,
 };
 
-GLuint vtxindex[]={
+GLuint vtxindex[] = {
     0,1,2,3
 };
 
 float start_color_const[] = { 0.0, 0.0, 1.0, 1.0 };
 float end_color_const[] = { 1.0, 1.0, 1.0, 1.0 };
 float center_point_const[] = {-0.5, 0.0, 0.0, 0.0};
-float zoom_value = 1.0;
+int zoom_value = 7;
 static int is_dragging = 0;
 static int drag_start_x;
 static int drag_start_y;
 
-static int make_resources(void)
+int compile_shader(GLuint shader, char *file);
+void printShaderInfoLog(GLuint shader);
+void printProgramInfoLog(GLuint program);
+int load_shader(char *vtxShdName, char *frgShdName, GLuint *lpProg);
+void screen_to_real(int x, int y, float* fx, float* fy);
+void onKey(int key, int x, int y);
+void onClick(int button, int state, int x, int y);
+void onMove(int x, int y);
+void force_redraw(int value);
+int make_resources(void);
+void render(void);
+
+int make_resources()
 {
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
@@ -43,7 +57,7 @@ static int make_resources(void)
     return 1;
 }
 
-static void render(void)
+void render()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     
@@ -57,7 +71,7 @@ static void render(void)
     GLint center = glGetUniformLocation(program, "center");
     glUniform4fv(center, 1, center_point_const);
     GLint zoom = glGetUniformLocation(program, "zoom");
-    glUniform1f(zoom, zoom_value);
+    glUniform1f(zoom, exp(zoom_value * log(1.1)));
     GLint aspect_ratio = glGetUniformLocation(program, "aspect_ratio");
     glUniform1f(aspect_ratio, 1200.0 / 900.0);
     
@@ -65,30 +79,17 @@ static void render(void)
     
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glVertexPointer(3, GL_FLOAT, 0, 0);    
-    glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_INT, (void*)vtxindex);
+    glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_INT, static_cast<void*>(vtxindex));
     
     glDisableClientState(GL_VERTEX_ARRAY);
     
     glutSwapBuffers();
 }
 
-static int loadShader(GLuint shader, char *file);
-static void printShaderInfoLog(GLuint shader);
-static void printProgramInfoLog(GLuint program);
-int FUTL_LoadShader(char *vtxShdName, char *frgShdName, GLuint *lpProg);
-
-/* シェーダコンパイルのログバッファ */
-#define MAX_SHADER_LOG_SIZE		(1024)
-char s_logBuffer[MAX_SHADER_LOG_SIZE]; 
-
 /*
  * シェーダプログラムのロード
  */
-int FUTL_LoadShader(
-                    char *vtxShdName,
-                    char *frgShdName,
-                    GLuint *lpProg
-                    )
+int load_shader(char *vtxShdName, char *frgShdName, GLuint *lpProg)
 {
 	GLuint vtxShader;
 	GLuint frgShader;
@@ -100,13 +101,13 @@ int FUTL_LoadShader(
 	frgShader = glCreateShader(GL_FRAGMENT_SHADER);
     
 	/* バーテックスシェーダのロードとコンパイル */
-	if (loadShader(vtxShader, vtxShdName) < 0)
+	if (compile_shader(vtxShader, vtxShdName) < 0)
 	{
 		return -1;
 	}
     
 	/* フラグメントシェーダのロードとコンパイル */
-	if (loadShader(frgShader, frgShdName) < 0)
+	if (compile_shader(frgShader, frgShdName) < 0)
 	{
 		return -1;
 	}
@@ -140,14 +141,10 @@ int FUTL_LoadShader(
 	return 0;
 }
 
-
 /*
  * シェーダープログラムをロードし、コンパイル
  */
-static int loadShader(
-                      GLuint shader, 
-                      char *name
-                      )
+int compile_shader(GLuint shader, char *name)
 {
 	GLint compiled;
     
@@ -183,7 +180,7 @@ static int loadShader(
 }
 
 /* シェーダコンパイルエラーの出力 */
-static void printShaderInfoLog(GLuint shader)
+void printShaderInfoLog(GLuint shader)
 {
 	int logSize;
 	int length;
@@ -193,6 +190,7 @@ static void printShaderInfoLog(GLuint shader)
     
 	if (logSize > 1)
 	{
+        char s_logBuffer[MAX_SHADER_LOG_SIZE];
 		glGetShaderInfoLog(shader, MAX_SHADER_LOG_SIZE, &length, s_logBuffer);
         std::cout << "Shader Info Log " << s_logBuffer << std::endl;
 	}
@@ -200,7 +198,7 @@ static void printShaderInfoLog(GLuint shader)
 
 
 /* シェーダリンクエラーの出力 */
-static void printProgramInfoLog(GLuint program)
+void printProgramInfoLog(GLuint program)
 {
 	int logSize;
 	int length;
@@ -210,29 +208,32 @@ static void printProgramInfoLog(GLuint program)
     
 	if (logSize > 1)
 	{
+        char s_logBuffer[MAX_SHADER_LOG_SIZE];
 		glGetProgramInfoLog(program, MAX_SHADER_LOG_SIZE, &length, s_logBuffer);
-		fprintf(stderr, "Program Info Log\n%s\n", s_logBuffer);
+        std::cout << "Program Info Log " << s_logBuffer << std::endl;
 	}
 }
 
-void screen_to_real(int x, int y, float* fx, float* fy) {
-    *fx = zoom_value * 2.0 / 1200 * x + center_point_const[0] - zoom_value;
-    *fy = zoom_value * 2.0 / 900 * (900 - y) + center_point_const[1] - zoom_value;
+void screen_to_real(int x, int y, float* fx, float* fy)
+{
+    *fx = exp(zoom_value * log(1.1)) * 2.0 / 1200 * x + center_point_const[0] - exp(zoom_value * log(1.1));
+    *fy = exp(zoom_value * log(1.1)) * 2.0 / 900 * (900 - y) + center_point_const[1] - exp(zoom_value * log(1.1));
 }
 
-void onKey(int key, int x, int y) {
+void onKey(int key, int x, int y)
+{
     float fx, fy;
     screen_to_real(x, y, &fx, &fy);
     switch (key) {
         case GLUT_KEY_UP:
             center_point_const[0] = fx;
             center_point_const[1] = fy;
-            zoom_value /= 1.1;
+            zoom_value--;
             break;
         case GLUT_KEY_DOWN:
             center_point_const[0] = fx;
             center_point_const[1] = fy; 
-            zoom_value *= 1.1;
+            zoom_value++;
 
             break;
         default:
@@ -240,9 +241,8 @@ void onKey(int key, int x, int y) {
     }
 }
 
-
-
-void onClick(int button, int state, int x, int y) {
+void onClick(int button, int state, int x, int y)
+{
     if (button == GLUT_LEFT_BUTTON) {
         if (state == GLUT_DOWN) {
             is_dragging = 1;
@@ -254,10 +254,11 @@ void onClick(int button, int state, int x, int y) {
     }
 }
 
-void onMove(int x, int y) {
+void onMove(int x, int y)
+{
     if (is_dragging) {
-        float fx = (x - drag_start_x) / 600.0 * zoom_value;
-        float fy = (y - drag_start_y) / 450.0 * zoom_value;
+        float fx = (x - drag_start_x) / 600.0 * exp(zoom_value * log(1.1));
+        float fy = (y - drag_start_y) / 450.0 * exp(zoom_value * log(1.1));
         
         drag_start_x = x;
         drag_start_y = y;
@@ -267,14 +268,15 @@ void onMove(int x, int y) {
     }
 }
 
-void force_redraw(int value) {
+void force_redraw(int value)
+{
     glutPostRedisplay();
     glutTimerFunc(20, force_redraw, 0);
 }
 
 int main (int argc, const char * argv[])
 {
-    glutInit(&argc, (char**)argv);
+    glutInit(&argc, const_cast<char**>(argv));
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
     glutInitWindowSize(1200, 900);
     glutCreateWindow("Mandelbrot Renderer");
@@ -286,10 +288,10 @@ int main (int argc, const char * argv[])
     glutMouseFunc(onClick);
     glutMotionFunc(onMove);
     
-    FUTL_LoadShader("test_vertex.glsl", "test_fragment.glsl", &program);
+    load_shader("test_vertex.glsl", "test_fragment.glsl", &program);
     
     if (!make_resources()) {
-        fprintf(stderr, "Failed to load resources\n");
+        std::cout << "Failed to prepare resources" << std::endl;
         return 1;
     }
     
