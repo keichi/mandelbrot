cc13b7229c26e7e64a8f62d2a1693ee0af49c1a8
Keichi Takahashi <keichi.t@me.com>
2012-03-14 16:42:52 +0900

    Initial Commit

---
+++ mandelbrot/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  mandelbrot
+//
+//  Created by Keichi Takahashi on 12/03/14.
+//  Copyright (c) 2012年 Osaka University. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
